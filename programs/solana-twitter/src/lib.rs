b5e660ab117e6cfcdfd706c395acef12ffb045a3
yoshiioko <acardoza@pdx.edu>
2022-06-02 11:46:27 -0700

    Define the Tweet account structure and constants needed to determine its size.

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -13,3 +13,31 @@ pub mod solana_twitter {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+// 1. Define the structure of the Tweet account.
+#[account]
+pub struct Tweet {
+    pub author: Pubkey,
+    pub timestamp: i64,
+    pub topic: String,
+    pub content: String,
+}
+
+// 2. Add some useful constants for sizing properties
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4;
+const MAX_TOPIC_LENGTH: usize = 50 * 4;
+const MAX_CONTENT_LENGTH: usize = 280 * 4;
+
+// 3. Add a constant on the Tweet acount that provides its total size.
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH
+        + TIMESTAMP_LENGTH
+        + STRING_LENGTH_PREFIX
+        + MAX_TOPIC_LENGTH
+        + STRING_LENGTH_PREFIX
+        + MAX_CONTENT_LENGTH;
+}
