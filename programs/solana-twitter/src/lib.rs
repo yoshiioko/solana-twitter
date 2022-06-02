9e6cf69114fbad4df37278080b114d87f1326983
yoshiioko <acardoza@pdx.edu>
2022-06-02 12:32:36 -0700

    Fixing System Program for newer Anchor version.

--- programs/solana-twitter/src/lib.rs
+++ programs/solana-twitter/src/lib.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::system_program;
 
 declare_id!("6jRCggDP41Qe5Rc5ECCgbQP5tvDEqxf2hSyWFdW71tzc");
 
@@ -35,8 +34,7 @@ pub struct SendTweet<'info> {
     pub tweet: Account<'info, Tweet>,
     #[account(mut)]
     pub author: Signer<'info>,
-    #[account(address = system_program::ID)]
-    pub system_program: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 // 1. Define the structure of the Tweet account.
