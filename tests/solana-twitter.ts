18b22d9ac69356a736799f9f17ca7b922896e66c
yoshiioko <acardoza@pdx.edu>
2022-06-02 16:29:22 -0700

    Created initial happy tests for contract

--- tests/solana-twitter.ts
+++ tests/solana-twitter.ts
@@ -1,16 +1,82 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { SolanaTwitter } from "../target/types/solana_twitter";
+import * as assert from "assert";
 
 describe("solana-twitter", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.SolanaTwitter as Program<SolanaTwitter>;
+  const program = anchor.workspace.SolanaTwitter;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Can send a new tweet", async () => {
+    const tweet = anchor.web3.Keypair.generate();
+
+    await program.rpc.sendTweet("veganism", "Hummus, am I right?", {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    });
+
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    assert.equal(
+      tweetAccount.author.toBase58(),
+      program.provider.wallet.publicKey.toBase58()
+    );
+    assert.equal(tweetAccount.topic, "veganism");
+    assert.equal(tweetAccount.content, "Hummus, am I right?");
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it("Can send a new tweet without a topic", async () => {
+    const tweet = anchor.web3.Keypair.generate();
+
+    await program.rpc.sendTweet("", "gm", {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [tweet],
+    });
+
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    assert.equal(
+      tweetAccount.author.toBase58(),
+      program.provider.wallet.publicKey.toBase58()
+    );
+    assert.equal(tweetAccount.topic, "");
+    assert.equal(tweetAccount.content, "gm");
+    assert.ok(tweetAccount.timestamp);
+  });
+
+  it("Can send a new tweet from a different author", async () => {
+    const otherUser = anchor.web3.Keypair.generate();
+    const signature = await program.provider.connection.requestAirdrop(
+      otherUser.publicKey,
+      1000000000
+    );
+    await program.provider.connection.confirmTransaction(signature);
+
+    const tweet = anchor.web3.Keypair.generate();
+
+    await program.rpc.sendTweet("veganism", "Hummus, am I right?", {
+      accounts: {
+        tweet: tweet.publicKey,
+        author: otherUser.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [otherUser, tweet],
+    });
+
+    const tweetAccount = await program.account.tweet.fetch(tweet.publicKey);
+    assert.equal(
+      tweetAccount.author.toBase58(),
+      otherUser.publicKey.toBase58()
+    );
+    assert.equal(tweetAccount.topic, "veganism");
+    assert.equal(tweetAccount.content, "Hummus, am I right?");
+    assert.ok(tweetAccount.timestamp);
   });
 });
