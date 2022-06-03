458b26955137d7587dae15530c3b7503e0ffe1fb
yoshiioko <acardoza@pdx.edu>
2022-06-02 18:09:31 -0700

    Adding scaffolding for Frontend

---
+++ app/src/main.js
@@ -0,0 +1,15 @@
+// CSS.
+import "./main.css";
+
+// Routing.
+import { createRouter, createWebHashHistory } from "vue-router";
+import routes from "./routes";
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+import { createApp } from "vue";
+import App from "./App.vue";
+
+createApp(App).use(router).mount("#app");
