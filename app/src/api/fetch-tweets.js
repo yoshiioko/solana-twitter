b416a881e98afb9d9cf7165d856efa21f300af2a
yoshiioko <acardoza@pdx.edu>
2022-06-04 16:26:21 -0700

    implement helpers to easily get filters.

--- app/src/api/fetch-tweets.js
+++ app/src/api/fetch-tweets.js
@@ -1,8 +1,23 @@
 import { useWorkspace } from "@/composables";
 import { Tweet } from "@/models";
+import bs58 from "bs58";
 
-export const fetchTweets = async function () {
+export const fetchTweets = async function (filters = []) {
   const { program } = useWorkspace();
-  const tweets = await program.value.account.tweet.all();
+  const tweets = await program.value.account.tweet.all(filters);
   return tweets.map((tweet) => new Tweet(tweet.publicKey, tweet.account));
 };
+
+export const authorFilter = (authorBase58PublicKey) => ({
+  memcmp: {
+    offset: 8,
+    bytes: authorBase58PublicKey,
+  },
+});
+
+export const topicFilter = (topic) => ({
+  memcmp: {
+    offset: 8 + 32 + 8 + 4,
+    bytes: bs58.encode(Buffer.from(topic)),
+  },
+});
