cb8eb0fcc31872b8ec287db2a562db2a4477e8a6
yoshiioko <acardoza@pdx.edu>
2022-06-05 12:48:22 -0700

    Fixes home page not fetching all tweets.

--- app/src/components/PageHome.vue
+++ app/src/components/PageHome.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watchEffect } from "vue";
 import { fetchTweets } from "@/api";
 import TweetForm from "@/components/TweetForm";
 import TweetList from "@/components/TweetList";
+import { useWorkspace } from "@/composables";
+
 const tweets = ref([]);
 const loading = ref(true);
-fetchTweets()
-  .then((fetchedTweets) => (tweets.value = fetchedTweets))
-  .finally(() => (loading.value = false));
+const { wallet } = useWorkspace();
+
+watchEffect(() => {
+  if (!wallet.value) return;
+  fetchTweets()
+    .then((fetchedTweets) => (tweets.value = fetchedTweets))
+    .finally(() => (loading.value = false));
+});
+
 const addTweet = (tweet) => tweets.value.push(tweet);
 </script>
 
