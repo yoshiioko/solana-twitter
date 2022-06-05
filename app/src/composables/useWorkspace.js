bf9e747842da729c15a2837b6fa7429f63747a8a
yoshiioko <acardoza@pdx.edu>
2022-06-05 00:04:38 -0700

    Updating to be able to deploy frontend to local, devnet, and mainnet

--- app/src/composables/useWorkspace.js
+++ app/src/composables/useWorkspace.js
@@ -2,8 +2,9 @@ import { computed } from "vue";
 import { useAnchorWallet } from "solana-wallets-vue";
 import { Connection, PublicKey } from "@solana/web3.js";
 import { AnchorProvider, Program } from "@project-serum/anchor";
-import idl from "../../../target/idl/solana_twitter.json";
+import idl from "@/idl/solana_twitter.json";
 
+const clusterUrl = process.env.VUE_APP_CLUSTER_URL;
 const preflightCommitment = "processed";
 const commitment = "processed";
 const programID = new PublicKey(idl.metadata.address);
@@ -13,7 +14,7 @@ export const useWorkspace = () => workspace;
 
 export const initWorkspace = () => {
   const wallet = useAnchorWallet();
-  const connection = new Connection("http://127.0.0.1:8899", commitment);
+  const connection = new Connection(clusterUrl, commitment);
   const provider = computed(() => {
     return new AnchorProvider(connection, wallet.value, {
       preflightCommitment,
